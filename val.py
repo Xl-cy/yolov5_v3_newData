d5b5449cf18b41fa34b09bb4856dfe861a8b2969
Xl-cy <1060425196@qq.com>
2022-04-22 17:06:23 +0800

    commit

--- val.py
+++ val.py
@@ -333,8 +333,8 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='dataset.yaml path')
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/bddvehicle.yaml', help='dataset.yaml path')
+    parser.add_argument('--weights', nargs='+', type=str, default=0, help='model.pt path(s)')
     parser.add_argument('--batch-size', type=int, default=32, help='batch size')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='confidence threshold')
